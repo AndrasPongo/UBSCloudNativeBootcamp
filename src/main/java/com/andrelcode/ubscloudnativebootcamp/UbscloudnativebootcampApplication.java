b7ed61c63d4bb1831a809ffd319bae2f6c758fb4
AndrasPongo <andras.pongo14@gmail.com>
2020-10-10 15:04:32 +0800

    Initial commit

---
+++ src/main/java/com/andrelcode/ubscloudnativebootcamp/UbscloudnativebootcampApplication.java
@@ -0,0 +1,13 @@
+package com.andrelcode.ubscloudnativebootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbscloudnativebootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbscloudnativebootcampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrelcode/ubscloudnativebootcamp/UbscloudnativebootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelcode.ubscloudnativebootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbscloudnativebootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
